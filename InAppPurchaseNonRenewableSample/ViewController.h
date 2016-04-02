f94e37f74f1031e804cf21383895a9bc2261e179
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2016-04-03 02:09:38 +0900

    initialize

---
+++ InAppPurchaseNonRenewableSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InAppPurchaseNonRenewableSample
+//
+//  Created by hirauchi.shinichi on 2016/04/03.
+//  Copyright © 2016年 SAPPOROWORKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InAppPurchaseNonRenewableSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InAppPurchaseNonRenewableSample
+//
+//  Created by hirauchi.shinichi on 2016/04/03.
+//  Copyright © 2016年 SAPPOROWORKS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
